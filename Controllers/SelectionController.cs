dd919904e4ac3d339aa552dbe88a0b1344e8d000
sahaya-subisha <82675416+sahaya-subisha@users.noreply.github.com>
2022-03-15 21:46:28 +0530

    Add files via upload

---
+++ Controllers/SelectionController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class SelectionController : Controller
+    {
+        // GET: Selection
+        public ActionResult Selection1(string Selection1)
+        {
+            ViewBag.Selection1 = Selection1;
+            return View();
+        }
+        public ActionResult Selection2(string Selection2)
+        {
+            ViewBag.Selection2 = Selection2;
+            return View();
+        }
+        public ActionResult Selection3(string Selection3)
+        {
+            ViewBag.Selection3 = Selection3;
+            return View();
+        }
+        public ActionResult Selection()
+        {
+            return View();
+        }
+        public ActionResult ConsolePannel(string Selection)
+        {
+            ViewBag.Selection = Selection;
+            return View();
+        }
+       
+    }
+}
